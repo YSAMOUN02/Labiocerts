d86493add7e29faa98c84e73410e72c2cb62bed7
Y Samoun <itsupport1@ppmpharma.com>
2024-06-20 17:15:07 +0700

    merged

--- labiocert/routes/web.php
+++ labiocert/routes/web.php
@@ -8,16 +8,32 @@ use App\Http\Controllers\frontend\LocationController;
 use App\Http\Controllers\backend\adminController;
 use App\Http\Controllers\backend\logo_clientController;
 use App\Http\Controllers\backend\service_backendController;
-
+use App\Http\Controllers\backend\advertisingController;
+use App\Http\Controllers\backend\quoteController;
 use Illuminate\Support\Facades\Route;
 
 
 
 
+// Back End
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/login', [adminController::class, 'login']);
+Route::get('/admin', [adminController::class, 'admin']);
+Route::get('/admin/service', [service_backendController::class, 'service_show']);
+Route::get('/admin/service/category', [service_backendController::class, 'service_category']);
+Route::get('/admin/service/category/parameter', [service_backendController::class, 'service_parameter']);
+Route::get('/admin/logoClient', [logo_clientController::class, 'logoClient']);
+
+Route::get('/admin/advertising', [advertisingController::class, 'advertising_show']);
+
+Route::get('/admin/quote/view', [quoteController::class, 'quote_show']);
+
+
+
+
+
+
+// frontend
 
 Route::get('/', [HomeController::class, 'homepage']);
 Route::get('/welcomelabiocert', [HomeController::class, 'welcomelabiocert']); 
