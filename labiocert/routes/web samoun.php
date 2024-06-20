b7f36a9bf2b93d8546d264d2a1ea7a13582dcf1a
Y Samoun <itsupport1@ppmpharma.com>
2024-06-20 07:59:30 +0700

    20-6-2024

--- labiocert/routes/web samoun.php
+++ labiocert/routes/web samoun.php
@@ -1,22 +1,28 @@
 <?php
 
+use App\Http\Controllers\frontend\HomeController;
+use App\Http\Controllers\frontend\ServiceController; 
+use App\Http\Controllers\frontend\MediaController;
+use App\Http\Controllers\frontend\LocationController;
+
 use App\Http\Controllers\backend\adminController;
 use App\Http\Controllers\backend\logo_clientController;
 use App\Http\Controllers\backend\service_backendController;
 
-use App\Http\Controllers\frontend\HomeController;
-use App\Http\Controllers\frontend\ServiceController;
-
 use Illuminate\Support\Facades\Route;
+
+
+
+
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-
 Route::get('/', [HomeController::class, 'homepage']);
 Route::get('/service', [ServiceController::class, 'service']);
-
+Route::get('/media', [MediaController::class, 'media']);
+Route::get('/location', [LocationController::class, 'location']);
 
 
 // Backend 
--- labiocert/routes/web.php
+++ labiocert/routes/web.php
@@ -4,7 +4,16 @@ use App\Http\Controllers\frontend\HomeController;
 use App\Http\Controllers\frontend\ServiceController; 
 use App\Http\Controllers\frontend\MediaController;
 use App\Http\Controllers\frontend\LocationController;
+
+use App\Http\Controllers\backend\adminController;
+use App\Http\Controllers\backend\logo_clientController;
+use App\Http\Controllers\backend\service_backendController;
+
 use Illuminate\Support\Facades\Route;
+
+
+
+
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -15,3 +24,12 @@ Route::get('/service', [ServiceController::class, 'service']);
 Route::get('/media', [MediaController::class, 'media']);
 Route::get('/location', [LocationController::class, 'location']);
 
+
+// Backend 
+
+Route::get('/login', [adminController::class, 'login']);
+Route::get('/admin', [adminController::class, 'admin']);
+Route::get('/admin/service', [service_backendController::class, 'service_show']);
+Route::get('/admin/service/category', [service_backendController::class, 'service_category']);
+Route::get('/admin/service/category/parameter', [service_backendController::class, 'service_parameter']);
+Route::get('/admin/logoClient', [logo_clientController::class, 'logoClient']);
